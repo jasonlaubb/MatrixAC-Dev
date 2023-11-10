8430ead96e747c1e6b6c039f8e0ea92bf0883bca
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-11-10 22:44:07 +0800

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @author jasonlaubb
+ * @contributors ravriv, RaMiGamerDev, notthinghere
+ * @license AGPLv3
+ * @github https://github.com/jasonlaubb/Matrix-AntiCheat
+ */
+
+import "./Modules/Combat/Auto Clicker"
+import "./Modules/Combat/Kill Aura"
+import "./Modules/Combat/Reach"
+import "./Modules/Misc/Spammer"
+import "./Modules/Movement/Fly"
+import "./Modules/Movement/Phase"
+import "./Modules/Movement/Speed"
+import "./Modules/World/Nuker"
+import "./Modules/World/Scaffold"
