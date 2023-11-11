bdff40e1b37945e7679626638d704b286a5fe473
Raven Rivera <125807666+ravriv@users.noreply.github.com>
2023-11-12 06:12:19 +0800

    No need for entityHurt

--- src/Modules/Movement/Fly.ts
+++ src/Modules/Movement/Fly.ts
@@ -1,20 +1,7 @@
-import {
-    world,
-    system,
-    Player,
-    EntityDamageCause,
-    Vector3,
-    Dimension
-} from "@minecraft/server";
+import { world, system, Vector3, Dimension } from "@minecraft/server";
 
 import config from "../../Data/Config";
 
-class FallData {
-    count: number;
-    lastFallDamageTime: number;
-}
-
-const fallData = new Map<string, FallData>();
 const previousLocations = new Map<string, Vector3>();
 import { flag, isAdmin } from "../../Assets/Util";
 import { MinecraftBlockTypes } from "../../node_modules/@minecraft/vanilla-data/lib/index";
@@ -98,42 +85,7 @@ system.runInterval(() => {
     }
 })
 
-world.afterEvents.entityHurt.subscribe(({ hurtEntity, damageSource }) => {
-    const toggle: boolean = (world.getDynamicProperty("antiFly") ?? config.antiFly.enabled) as boolean;
-    if (toggle !== true) return;
-
-    const player = hurtEntity;
-    const damageCause = damageSource.cause;
-
-    if (!(player instanceof Player)) return
-    if (isAdmin(player)) return
-
-    if (player.isFalling && damageCause === EntityDamageCause.fall) {
-        const { id } = player;
-
-        const currentfallData = fallData.get(id) || { count: 0, lastFallDamageTime: 0 };
-        const currentTime = Date.now();
-        if (currentTime - currentfallData.lastFallDamageTime < config.antiFly.minFallInterval) {
-            currentfallData.count++;
-        } else {
-            currentfallData.count = 1;
-        }
-        currentfallData.lastFallDamageTime = currentTime;
-        fallData.set(id, currentfallData);
-
-        if (currentfallData.count >= config.antiFly.maxFallCount) {
-            const prevLoc = previousLocations.get(id);
-            if (prevLoc) {
-                player.teleport(prevLoc);
-                flag(player, "Fly", config.antiFly.punishment, ["type:Invalid Fall Damage"])
-                fallData.delete(id);
-            }
-        }
-    }
-});
-
 world.afterEvents.playerLeave.subscribe(({ playerId }) => {
     const id = playerId;
-    fallData.delete(id);
     previousLocations.delete(id);
 })
