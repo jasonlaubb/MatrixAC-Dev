943ee9c12fd3456bfd424d401e297091bca559ba
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-11-12 17:36:23 +0800

    Fixed freeze don't work

--- src/Functions/moderateModel/freezeHandler.ts
+++ src/Functions/moderateModel/freezeHandler.ts
@@ -13,30 +13,32 @@ class Freeze {
 
 system.runInterval(() => {
     for (const player of world.getAllPlayers()) {
-        if (isAdmin (player)) continue;
+        if (isAdmin(player)) continue;
         let freezeInfo = player.getDynamicProperty("freeze") as string | undefined | Freeze;
-        if (freezeInfo === undefined) return
+        if (freezeInfo === undefined) continue;
 
         freezeInfo = JSON.parse(freezeInfo as string) as Freeze;
 
-        const freeze = freezeInfo as Freeze
+        const freeze = freezeInfo as Freeze;
 
         player.teleport(freeze.location, {
-        dimension: world.getDimension(freeze.dimension)
-        })
-        player.addEffect("minecraft:slowness", 2, { amplifier: 255, showParticles: false })
+            dimension: world.getDimension(freeze.dimension)
+        });
+        player.addEffect("minecraft:slowness", 2, { amplifier: 255, showParticles: false });
     }
-}, 20)
+}, 20);
 
-function freeze (player: Player) {
+function freeze(player: Player) {
     const floorPos = {
         x: Math.floor(player.location.x),
         y: Math.floor(player.location.y),
         z: Math.floor(player.location.z)
-    } as Vector3
-    if (player.getDynamicProperty("freeze") !== undefined) return false
-    system.run(() => player.setDynamicProperty("freeze", JSON.stringify({ location: floorPos, dimension: player.dimension })))
-    return true
+    } as Vector3;
+    if (player.getDynamicProperty("freeze") !== undefined) return false;
+    system.run(() => {
+        player.setDynamicProperty("freeze", JSON.stringify({ location: floorPos, dimension: player.dimension }));
+    });
+    return true;
 }
 
 function unfreeze (player: Player) {
