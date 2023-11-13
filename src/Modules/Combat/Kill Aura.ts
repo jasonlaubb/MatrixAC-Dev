f56db1f4c3028e3fd213ad242e09167a0982a051
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-11-13 20:00:32 +0800

    Update Kill Aura.ts

--- src/Modules/Combat/Kill Aura.ts
+++ src/Modules/Combat/Kill Aura.ts
@@ -63,20 +63,7 @@ function calculateMagnitude({ x, y, z }: Vector3) {
     return Math.sqrt(x ** 2 + y ** 2 + z ** 2);
 }
 
-function calculateAngle(v1: Vector3, v2: Vector3) {
-    const { x: x1, y: y1, z: z1 } = v1;
-    const { x: x2, y: y2, z: z2 } = v2;
-    const dotProduct: number = x1 * x2 + y1 * y2 + z1 * z2;
-    const m1: number = calculateMagnitude(v1);
-    const m2: number = calculateMagnitude(v2);
-    const denominator: number = m1 * m2;
-
-    if (denominator === 0) return 0;
-
-    const radians: number = Math.acos(dotProduct / denominator);
-    const degrees: number = radians * (180 / Math.PI);
-    return degrees;
-}
+const calculateAngle = (pos1, pos2, rotation = -90) => (Math.atan2((pos2.z - pos1.z), (pos2.x - pos1.x)) * 180 / Math.PI - rotation - 90 + 360) % 360;
 
 world.afterEvents.entityHitEntity.subscribe(({ damagingEntity, hitEntity }) => {
     const toggle = (world.getDynamicProperty("antiKillAura") ?? config.antiKillAura.enabled) as boolean;
