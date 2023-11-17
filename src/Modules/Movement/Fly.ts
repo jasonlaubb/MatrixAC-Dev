2c68c1caafe590cfaacbe15dec13b31d7d4320bd
RaMiGamer <140818153+RaMiGamerDev@users.noreply.github.com>
2023-11-17 17:24:17 +0100

    Fixed this rubbish anti fly

--- src/Modules/Movement/Fly.ts
+++ src/Modules/Movement/Fly.ts
@@ -4,7 +4,7 @@ import config from "../../Data/Config";
 import { MinecraftEffectTypes } from "../../node_modules/@minecraft/vanilla-data/lib/index";
 
 const previousLocations = new Map();
-
+const velocityRecorder = new Map();
 /**
  * @author RaMiGamerDev & ravriv
  * @description This checks if a player velocity is too high.
@@ -16,24 +16,48 @@ async function antiFly (player: Player, now: number) {
     const jumpEffect = player.getEffect(MinecraftEffectTypes.JumpBoost)
     const prevLoc = previousLocations.get(id);
     const { x, y, z } = player.getVelocity();
-    const xz = Math.hypot(x, z);
 
     if (isFlying || isInWater || isGliding || player.hasTag("matrix:levitating") || (jumpEffect && jumpEffect.amplifier > 2) || (threwTridentAt && now - threwTridentAt < 3000) || (lastExplosionTime && now - lastExplosionTime < 5000)) {
         return;
     }
-
-    if (isOnGround) {
+    if (isOnGround && y == 0) {
         previousLocations.set(id, player.location);
     }
+    if (player.hasTag("matrix:slime") && velocityY <= 0) {
+            player.removeTag("matrix:slime");
+    }
+    if (blockAround(player,"minecraft:slime") == true) {
+            player.addTag("matrix:slime");
+    }
     if (prevLoc) {
-        if ((y > 0.7 && xz > 0.39) || (y === 0 && xz > 0.39 && !player.isClimbing && !player.isOnGround)) {
+        if (y > 0.7 && player.hasTag("matrix:slime")) {
             player.teleport(prevLoc);
-            player.applyDamage(8);
+            player.applyDamage(0);
             flag (player, "Fly", config.antiFly.maxVL, config.antiFly.punishment, ["velocityY:" + y.toFixed(2), "velocityXZ:" + xz.toFixed(2)])
         }
     }
 }
+async function antiFlyA (player: Player, now: number) {
+    const { id, isOnGround, isFlying, isInWater, isGliding, threwTridentAt, lastExplosionTime } = player;
+    const jumpEffect = player.getEffect(MinecraftEffectTypes.JumpBoost)
+    const prevLoc = previousLocations.get(id);
+    const { x, y, z } = player.getVelocity();
 
+    if (isFlying || isInWater || isGliding || player.hasTag("matrix:levitating") || (jumpEffect && jumpEffect.amplifier > 2) || (threwTridentAt && now - threwTridentAt < 3000) || (lastExplosionTime && now - lastExplosionTime < 5000)) {
+        return;
+    }
+    if(y == 0){
+velocityRecorder.set(id,1)
+    }
+    if(y != 0){
+    velocityRecorder.set(id,0)
+    }
+    if(velocityRecorder.get(id) == 1){
+player.teleport(prevLoc);
+player.applyDamage(0);
+flag (player, "Fly", config.antiFly.maxVL, config.antiFly.punishment, ["velocityY:" + y.toFixed(2), "velocityXZ:" + xz.toFixed(2)])
+    }
+}
 system.runInterval(() => {
     const toggle: boolean = Boolean(world.getDynamicProperty("antiFly")) ?? config.antiFly.enabled;
     if (toggle !== true) return;
@@ -45,3 +69,12 @@ system.runInterval(() => {
         antiFly (player, now)
     }
 }, 1);
+system.runInterval(() => {
+    const toggle: boolean = Boolean(world.getDynamicProperty("antiFly")) ?? config.antiFly.enabled;
+    if (toggle !== true) return;
+
+    const now = Date.now();
+    for (const player of world.getPlayers({ excludeGameModes: [GameMode.creative, GameMode.spectator] })) {
+        antiFlyA(player, now)
+    }
+ },7)
