6b082c0799f7787ed4a8927407214a0f6a4d0290
jasonlaubb <136591629+jasonlaubb@users.noreply.github.com>
2023-11-19 13:11:24 +0800

    A piece of cake (####)

---
+++ Data/Help.ts
@@ -0,0 +1,57 @@
+export function helpList (p: string): string {
+    return [
+        `§g${p}help - Show this help message`,
+        `§g${p}toggles - Show all module's toggle`,
+        `§g${p}toggle <module> <enable/disable>- Toggle a module`,
+        `§g${p}op <player> - Give player admin permission`,
+        `§g${p}deop <player> - Remove player's admin permission`,
+        `§g${p}passwords <oldPassword> <newPassword> - Change the password`,
+        `§g${p}flagmode <all/tag/bypass/admin> - Change the flag mode`,
+        `§g${p}rank <set/add/remove> <player> <rank> - Change the rank on a player`,
+        `§g${p}rankclear <player> - Clear all rank on a player`,
+        `§g${p}defaultrank <rank> - Change the default rank`,
+        `§g${p}showallrank <true/false> - Show all rank in chat`,
+        `§g${p}ban <player> <reason> <timeRegax/forever> - Ban a player`,
+        `§g${p}unban <player> - Unban a player`,
+        `§g${p}unbanremove <player> - Remove a player from unban queue`,
+        `§g${p}unbanlist - Show all player in unban queue`,
+        `§g${p}freeze <player> - Freeze a player`,
+        `§g${p}unfreeze <player> - Unfreeze a player`,
+        `§g${p}vanish - Vanish yourself`,
+        `§g${p}unvanish - Unvanish yourself`,
+        `§g${p}invcopy <player> - Copy a player's inventory to yours`,
+        `§g${p}invsee <player> - View a player's inventory in a ui`,
+        `§g${p}echestwipe <player> - Wipe a player's ender chest`,
+        `§g${p}lockdowncode <get/set/random> <set: code>/[random: length] - Do with the lockdown code`,
+        `§g${p}lockdown <code> - Lockdown the server with a code`,
+        `§g${p}unlock <code> - Unlock the server`,
+        `§g${p}adminchat - switch between admin channel and public channel`
+    ].join("\n")
+}
+
+export function toggleList (p: string): string {
+    const moduleList = validModules
+    return moduleList.map((module) => `§g${p}toggle ${module} <enable/disable> - Toggle ${module} module`).join("\n")
+}
+
+export const validModules: string[] = [
+    "chatRank",
+    "dimensionLock",
+    "antiReach",
+    "antiKillAura",
+    "antiAutoClicker",
+    "antiSpam",
+    "antiFly",
+    "antiPhase",
+    "antiSpeed",
+    "antiNuker",
+    "antiSurround",
+    "antiTower",
+    "antiNoSlow",
+    "antiBlockReach",
+    "antiAim",
+    "antiScaffold",
+    "antiIllegalItem",
+    "antiNameSpoof",
+    "antiOperator"
+]
